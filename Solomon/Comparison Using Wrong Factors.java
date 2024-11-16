c6abd800c54aff3ea4e9fb56669f811300f37af7
SMHUDS1 <smhuds1@ilstu.edu>
2024-11-16 12:57:28 -0600

    Comparison Using Wrong Factors.java

--- Solomon/Comparison Using Wrong Factors.java
+++ Solomon/Comparison Using Wrong Factors.java
@@ -1,12 +1,10 @@
-#include <iostream>
-#include <string>
-
-void compareValues(int id1, std::string name1, int id2, std::string name2) {
-    // Incorrect comparison of unrelated factors
-    if (id1 == name2.length()) { // Should compare id1 with id2 or name1 with name2
-        std::cout << "Match found (incorrect comparison)\n";
-    }
+public class CWE1025 {
+    public void compareValues(int id1, String name1, int id2, String name2) {
+        // Incorrect comparison of unrelated factors
+        if (id1 == name2.length()) { // Should compare id1 with id2 or name1 with name2
+            System.out.println("Match found (incorrect comparison)");
+        }
   if(name1 == name2)//Should be using the .equals method to compare string literals
-     std::cout << "Match found (incorrect comparison)\n";
+     System.out.println( "Match found (incorrect comparison)\n");
 }
 

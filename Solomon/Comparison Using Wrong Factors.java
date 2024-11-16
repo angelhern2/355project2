4e0a23938377d5cc978b9bc8b898deedf7bdacdd
SMHUDS1 <smhuds1@ilstu.edu>
2024-11-16 12:40:08 -0600

    Comparison Using Wrong Factors.java

--- Solomon/Comparison Using Wrong Factors.java
+++ Solomon/Comparison Using Wrong Factors.java
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <string>
+
+void compareValues(int id1, std::string name1, int id2, std::string name2) {
+    // Incorrect comparison of unrelated factors
+    if (id1 == name2.length()) { // Should compare id1 with id2 or name1 with name2
+        std::cout << "Match found (incorrect comparison)\n";
+    }
+  if(name1 == name2)//Should be using the .equals method to compare string literals
+     std::cout << "Match found (incorrect comparison)\n";
+}
 

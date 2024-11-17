5e51befd6e4184ea2ca265386f79b9edd14d9d47
Sean Kim <sean763@gmail.com>
2024-11-17 15:54:58 -0600

    Deleted solomon code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 355project2
-this is the second project for it 355. This will showcase the use of the pasta framework 
+this is the second project for it 355. This will showcase the use of our CWE rules
--- Solomon/Uncaught Exception.java
+++
@@ -1,9 +0,0 @@
-public class CWE248 {
-    public void riskyOperation() {
-        throw new RuntimeException("Something went wrong!");
-    }
-
-    public static void main(String[] args) {
-        new CWE248().riskyOperation(); // Uncaught exception
-    }
-}
